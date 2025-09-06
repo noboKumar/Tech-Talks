2f3003fb3509df2ad93ad7198901cec64a749bce
noboKumar <nobokumar227@gmail.com>
2025-09-07 01:21:17 +0600

    add login Page

---
+++ app/login/page.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const Login = () => {
+  return (
+    <div className="space-y-5 border p-10 rounded-md shadow-md max-w-md mx-auto mt-20 bg-white">
+      <h1 className="text-4xl font-bold">Login Now</h1>
+      <form className="flex flex-col gap-4">
+        <div>
+          {/* email */}
+          <Label htmlFor="email">Email:</Label>
+          <Input type="email" id="email" placeholder="Email" name="email" />
+        </div>
+
+        <div>
+          {/* password */}
+          <Label htmlFor="password">Password:</Label>
+          <Input
+            type="password"
+            id="password"
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <Button type="submit">Login</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
