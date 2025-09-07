3a6b0b5fa9d289eddb08000758af38869b6c1cc6
noboKumar <nobokumar227@gmail.com>
2025-09-07 15:33:26 +0600

    add react hook form in login and register page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,36 +1,55 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit = (data: Inputs) => {
+    console.log(data);
+  };
+
   return (
     <div className="space-y-5 border p-10 rounded-md shadow-md max-w-md mx-auto mt-20 bg-card">
       <h1 className="text-4xl font-bold text-primary">Login Now</h1>
-      <form className="flex flex-col gap-2">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+        {/* email */}
         <div>
-          {/* email */}
           <Label htmlFor="email">Email:</Label>
           <Input
             type="email"
             id="email"
             placeholder="Email"
-            name="email"
-            required
+            {...register("email", { required: true })}
           />
         </div>
+        {errors.email && <span>This field is required</span>}
 
+        {/* password */}
         <div>
-          {/* password */}
           <Label htmlFor="password">Password:</Label>
           <Input
             type="password"
             id="password"
             placeholder="Password"
-            name="password"
-            required
+            {...register("password", { required: true })}
           />
         </div>
+        {errors.password && <span>This field is required</span>}
+
+        {/* don't have an account */}
         <p className="text-sm">
           Don&apos;t have an account?{" "}
           <Link
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,14 +1,31 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import React from "react";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  password: string;
+};
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit = (data: Inputs) => {
+    console.log(data);
+  };
   return (
     <div className="space-y-5 border p-10 rounded-md shadow-md max-w-md mx-auto mt-20 bg-card">
       <h1 className="text-4xl font-bold text-primary">Register Now</h1>
-      <form className="flex flex-col gap-2">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
         <div>
           {/* name */}
           <Label htmlFor="name">Name:</Label>
@@ -16,10 +33,10 @@ const Register = () => {
             type="text"
             id="name"
             placeholder="Name"
-            name="name"
-            required
+            {...register("name", { required: true })}
           />
         </div>
+        {errors.name && <span>This field is required</span>}
 
         <div>
           {/* File upload */}
@@ -28,7 +45,6 @@ const Register = () => {
             className="file:bg-card file:px-2 file:rounded-xl file:text-sm file:font-medium file:cursor-pointer"
             type="file"
             id="profile"
-            name="profile"
           />
         </div>
 
@@ -39,10 +55,10 @@ const Register = () => {
             type="email"
             id="email"
             placeholder="Email"
-            name="email"
-            required
+            {...register("email", { required: true })}
           />
         </div>
+        {errors.email && <span>This field is required</span>}
 
         <div>
           {/* password */}
@@ -51,10 +67,12 @@ const Register = () => {
             type="password"
             id="password"
             placeholder="Password"
-            name="password"
-            required
+            {...register("password", { required: true })}
           />
         </div>
+        {errors.password && <span>This field is required</span>}
+
+        {/* already have an account */}
         <p className="text-sm">
           Already have an account?{" "}
           <Link className="underline font-semibold text-blue-600" href="/login">
