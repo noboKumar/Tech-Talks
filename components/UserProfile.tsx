bcbcda18ec7b9a0f91f02228a251d61385627638
noboKumar <nobokumar227@gmail.com>
2025-09-09 01:55:09 +0600

    add user profile info left side menu

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,44 +1,66 @@
 import { getUserSession } from "@/lib/userSession";
+import { Link2 } from "lucide-react";
+import Link from "next/link";
+import {
+  FiHome,
+  FiBell,
+  FiUsers,
+  FiCalendar,
+  FiUser,
+  FiSettings,
+  FiHelpCircle,
+} from "react-icons/fi";
 
 const UserProfile = async () => {
   const user = await getUserSession();
+
+  const menuItems = [
+    { name: "Feed", icon: <FiHome />, href: "/feed" },
+    { name: "Profile", icon: <FiUser />, href: "/profile" },
+    { name: "Notification", icon: <FiBell />, href: "/notifications" },
+    { name: "Connections", icon: <FiUsers />, href: "/connections" },
+    { name: "Events", icon: <FiCalendar />, href: "/events" },
+    { name: "Settings", icon: <FiSettings />, href: "/settings" },
+    { name: "Help", icon: <FiHelpCircle />, href: "/help" },
+  ];
   return (
     <div>
       {/* user info */}
-      <div>
-        <h2>{user?.name}</h2>
-        <p>Web Developer</p>
-        <p>
+      <div className="text-center">
+        <h2 className="text-lg font-bold">{user?.name}</h2>
+        <p className="text-sm text-gray-400">Web Developer</p>
+        <p className="text-lg mt-2 text-gray-500">
           I would love to change the world, but they wont give me the source
           code.
         </p>
       </div>
       {/* follower stats */}
-      <div>
-        <div>
-          <span>20</span>
-          <span>Posts</span>
+      <div className="flex justify-around my-4">
+        <div className="flex flex-col text-center border-r-2 border-gray-300 pr-4">
+          <span className="font-bold">20</span>
+          <span className="text-gray-500">Posts</span>
         </div>
-        <div>
-          <span>100</span>
-          <span>Followers</span>
+        <div className="flex flex-col text-center border-r-2 border-gray-300 px-4">
+          <span className="font-bold">100</span>
+          <span className="text-gray-500">Followers</span>
         </div>
-        <div>
-          <span>50</span>
-          <span>Following</span>
+        <div className="flex flex-col text-center pl-4">
+          <span className="font-bold">50</span>
+          <span className="text-gray-500">Following</span>
         </div>
       </div>
-      <hr />
+      <hr className="my-4 border-gray-300 border" />
       {/* route */}
       <div>
         <ul>
-          <li>Feed</li>
-          <li>Notification</li>
-          <li>Connections</li>
-          <li>Events</li>
-          <li>Profile</li>
-          <li>Settings</li>
-          <li>Help</li>
+          {menuItems.map((item) => (
+            <Link key={item.name} href={item.href}>
+              <li className="flex items-center gap-3 my-3 font-medium cursor-pointer hover:text-blue-500">
+                {item.icon}
+                {item.name}
+              </li>
+            </Link>
+          ))}
         </ul>
       </div>
     </div>
